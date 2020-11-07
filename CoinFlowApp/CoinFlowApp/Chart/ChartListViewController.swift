c161194c041d2ccf9a70d33333eb2eef9c20a8a2
zombietux <>
2020-11-07 14:13:30 +0900

    Chart -> DetailChart 푸시 설정 예제

--- CoinFlowApp/CoinFlowApp/Chart/ChartListViewController.swift
+++ CoinFlowApp/CoinFlowApp/Chart/ChartListViewController.swift
@@ -15,15 +15,9 @@ class ChartListViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Chart", bundle: .main)
+        let chartDetailViewController = storyboard.instantiateViewController(identifier: "ChartDetailViewController")
+        navigationController?.pushViewController(chartDetailViewController, animated: true)
     }
-    */
-
 }

407325d6567e0f1f3e168240991d3571ad1655bf
zombietux <>
2020-11-07 12:23:38 +0900

    앱에 필요한 뷰컨트롤러 생성

---
+++ CoinFlowApp/CoinFlowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinFlowApp
+//
+//  Created by zombietux on 2020/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
